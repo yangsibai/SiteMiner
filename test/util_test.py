799dbf23107411ca861acc4b8a10f8de01efac70
yangsibai <yangsibai@gmail.com>
2014-09-30 16:41:41 +0800

    add some test method

---
+++ test/util_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'massimo'
+
+import unittest
+
+
+class UtilTestFunctions(unittest.TestCase):
+    def test_verify_url(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    UtilTestFunctions.main()

cd85eaeed527ed6c0bd05d52b7a11362930572a4
yangsibai <yangsibai@gmail.com>
2014-10-01 00:38:21 +0800

    can work now

--- Miner/Miner.py
+++ Miner/Miner.py
@@ -5,51 +5,65 @@ __author__ = 'massimo'
 import urllib2
 import util
 import thread
-import sched
-import datetime
+import sys
+import time
 
 
 class Miner:
     def __init__(self, url):
         self.start = url
-        self.tasks = {url: 0}
+        self.tasks = set()
+        self.tasks.add(url)
+        self.resolved = set()
         self.result = {}
-        self.shed = sched.scheduler
-        self.last_show_status_time = datetime.datetime.now()
 
     def run(self):
         thread.start_new_thread(self.status, ())
         while True:
             try:
-                link = self.tasks.popitem()
-                self.resolve(link[0])
+                link = self.tasks.pop()
+                self.resolve(link)
             except KeyError as e:
                 print e
                 break
 
     def status(self):
         while True:
-            if self.last_show_status_time < datetime.datetime.now() - datetime.timedelta(seconds=3):
-                self.last_show_status_time = datetime.datetime.now()
-                print self.result
+            print '-' * 30
+            for k, v in sorted(self.result.items()):
+                print k, len(v)
+            print '-' * 30
+            time.sleep(5)
 
     def resolve(self, url):
+        self.resolved.add(url)
         try:
             response = urllib2.urlopen(url)
-            self.result[url] = response.getcode()
+            code = response.getcode()
 
-            html = response.read()
-            all_links = util.pull_out_all_links(html)
-            if len(all_links) > 0:
-                for link in all_links:
-                    if link not in self.result and link not in self.tasks:
-                        self.tasks[link] = 0
+            all_links = util.pull_out_all_links(response.read())
+            [self.tasks.add(link) for link in all_links if link not in self.resolved]
 
             response.close()
         except urllib2.URLError as e:
-            if hasattr(e, "code"):
-                self.result[url] = e.code
             print e
+            if hasattr(e, "code"):
+                code = e.code
+            else:
+                code = -1
+        except:
+            print sys.exc_info()[0]
+            code = -1
+
+        self.add_result((url, code))
+
+    def add_result(self, res):
+        url = res[0]
+        code = res[1]
+        if code in self.result:
+            self.result[code].append(url)
+        else:
+            self.result[code] = [url]
 
 
 if __name__ == "__main__":
